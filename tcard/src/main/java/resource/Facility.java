f7f1e0ddcd2f08556ecd043f053f92a2e2d7ce2a
Jonah Mackey <jonahmackey699@gmail.com>
2021-11-07 21:11:18 -0500

    Started the facility class. I know this wasn't my job to do, but it is difficult to implement the criteria stuff without having something to work with (i.e. facility objects). I added a TODO for the displayFacilityInfo method as it is not implemented.

---
+++ tcard/src/main/java/resource/Facility.java
@@ -0,0 +1,40 @@
+package java.resource;
+
+public class Facility {
+    private String name;
+    private String address;
+    private String description;
+    private String hours;
+    private String criteria;
+
+    public Facility(String[] facilityInfo) {
+        // assuming facilityInfo has the following ordering of elements:
+        // {name, address, description, hours, criteria}
+        this.name = facilityInfo[0];
+        this.address = facilityInfo[1];
+        this.description = facilityInfo[2];
+        this.hours = facilityInfo[3];
+        this.criteria = facilityInfo[4];
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getDescription() { return this.description; }
+
+    public String getHours() {
+        return this.hours;
+    }
+
+    public String getCriteria() { return this.criteria; }
+
+    // TODO: implement this, should look similar to the displayProfile method in the Student and Faculty classes
+    public String displayFacilityInfo() {
+        return "";
+    }
+}

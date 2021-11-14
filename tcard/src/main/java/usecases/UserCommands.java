9a612a4864aaa032b0eee77e364645e6dea4326a
Jonah Mackey <jonahmackey699@gmail.com>
2021-11-14 16:52:58 -0500

    Updated UserCommands

--- tcard/src/main/java/usecases/UserCommands.java
+++ tcard/src/main/java/usecases/UserCommands.java
@@ -1,6 +1,8 @@
 package usecases;
 
 import entities.User;
+import entities.Student;
+import entities.Faculty;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -8,22 +10,31 @@ import java.util.ArrayList;
 public class UserCommands implements Serializable {
     private User user;
 
-    public UserCommands(ArrayList<String> userList) {
-        User user = this.createUser(userList);
+    public UserCommands(ArrayList<String> userInfo) {
+        User user = this.createUser(userInfo);
         this.user = user;
     }
 
     public String showProfile(){
-        return this.user.displayProfile();
+        if (this.user.getProfile().get(user.STATUS).equals("student")) {
+            return ((Student) this.user).displayProfile();
+        }
+        return ((Faculty) this.user).displayProfile();
+
     }
 
-    public User createUser(ArrayList<String> userList){
-
+    public User createUser(ArrayList<String> userInfo) {
+        if (userInfo.get(4).equals("student")) {
+            Student studentUser = new Student(userInfo);
+            return studentUser;
+        }
+        Faculty facultyUser = new Faculty(userInfo);
+        return facultyUser;
     }
 
-    public void changePassword(String oldpassword, String newpassword){
-        if (this.user.checkPassword(oldpassword)){
-            this.user.changePassword(newpassword);
+    public void changePassword(String oldPassword, String newPassword){
+        if (this.user.checkPassword(oldPassword)){
+            this.user.changePassword(newPassword);
         }
     }
 }

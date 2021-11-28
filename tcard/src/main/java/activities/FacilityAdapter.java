f9259b7edb7535cbdb8587a60c6759dbfce276f5
Jonah Mackey <jonahmackey699@gmail.com>
2021-11-28 03:42:34 -0500

    Minor refactoring to most of the files.
    The types of refactors I did are:
    - renaming/reformatting variable and method names to put in camel case
    - refactors to account for the changes made to UserDBHelper, RegisterActivity
    - deleted unused import statements
    - changed some variable/return types to satisfy the open closed principle. In particular, I made the change from ArrayList<String> variable = new ArrayList<String>(); to List<String> variable = new ArrayList<String>(); wherever it was applicable
    - dealing with miscellaneous yellow warnings throughout the files. There are still some that are present, though

--- tcard/src/main/java/activities/FacilityAdapter.java
+++ tcard/src/main/java/activities/FacilityAdapter.java
@@ -1,6 +1,5 @@
 package activities;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -8,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import usecases.FacilityMap;
 import controllers.FacilityManager;
 
 import java.io.IOException;

ab91629e2955c1ef211ddde0c6509590cbf22164
Jonah Mackey <jonahmackey699@gmail.com>
2021-12-03 14:29:10 -0500

    Added java doc for DiscountHelper use case class.

--- tcard/src/main/java/usecases/DiscountHelper.java
+++ tcard/src/main/java/usecases/DiscountHelper.java
@@ -9,8 +9,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Use case class that provides all necessary functionality relating to Discount objects.
+ */
 public class DiscountHelper {
 
+    public List<String> getApplicableDiscounts(List<Discount> discounts, User user) {
+        List<String> applicableDiscounts = new ArrayList<>();
+
+        for (Discount discount : discounts) {
+            if (discountApplicable(discount, user)) {
+                applicableDiscounts.add(discount.toString());
+            }
+        }
+        return applicableDiscounts;
+    }
+
+    /**
+     * Helper method that asses if a given student satisfies a discounts student criteria.
+     * @param studentDiscountCriteria a string containing a discounts student criteria.
+     * @param student a Student object
+     * @return true if the student satisfies the criteria, false if the student does not satisfy the criteria.
+     */
     private boolean studentDiscountApplicable(String studentDiscountCriteria, Student student) {
         String[] studentCriteriaArray = studentDiscountCriteria.split("\\.");
 
@@ -23,6 +43,12 @@ public class DiscountHelper {
         return (yearCriteriaSatisfied & programCriteriaSatisfied);
     }
 
+    /**
+     * Helper method that asses if a given faculty member satisfies a discounts faculty criteria.
+     * @param facultyDiscountCriteria a string containing a discounts faculty criteria.
+     * @param faculty a Faculty object
+     * @return true if the faculty satisfies the criteria, false if the faculty does not satisfy the criteria.
+     */
     private boolean facultyDiscountApplicable(String facultyDiscountCriteria, Faculty faculty) {
         String[] facultyCriteriaArray = facultyDiscountCriteria.split("\\.");
 
@@ -35,12 +61,23 @@ public class DiscountHelper {
         return (yearCriteriaSatisfied & programCriteriaSatisfied);
     }
 
+    /**
+     * Helper method to reformat a string containing conditions for a certain field of criteria as a list.
+     * @param subCriteria conditions for a certain field of criteria, represented as a string.
+     * @return A list of string, each string represents a condition for the field of criteria.
+     */
     private List<String> getSubCriteriaList(String subCriteria) {
         String[] subCriteriaArray = subCriteria.substring(subCriteria.indexOf("(") + 1, subCriteria.indexOf(")")).
                 split("/");
         return Arrays.asList(subCriteriaArray);
     }
 
+    /**
+     * Given a discount and user, this method determins if the user satisfies the discounts criteria.
+     * @param discount a Discount object
+     * @param user a User object
+     * @return true if the user satisfies the criteria, false if the user does not satisfy the criteria.
+     */
     private boolean discountApplicable(Discount discount, User user) {
         String[] discountCriteriaArray = discount.getCriteria().split(":");
 
@@ -49,16 +86,5 @@ public class DiscountHelper {
         }  else {
             return facultyDiscountApplicable(discountCriteriaArray[1], (Faculty) user);
         }
-    }
-
-    public List<String> getApplicableDiscounts(List<Discount> discounts, User user) {
-        List<String> applicableDiscounts = new ArrayList<>();
-
-        for (Discount discount : discounts) {
-            if (discountApplicable(discount, user)) {
-                applicableDiscounts.add(discount.toString());
-            }
-        }
-        return applicableDiscounts;
     }
 }

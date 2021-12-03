9cebd20b29b5e1d152f2182b3da7226c8c6a408a
Jonah Mackey <jonahmackey699@gmail.com>
2021-12-03 14:15:45 -0500

    Added java doc for MerchantMap use case class.

--- tcard/src/main/java/usecases/MerchantMap.java
+++ tcard/src/main/java/usecases/MerchantMap.java
@@ -5,12 +5,16 @@ import entities.Merchant;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Use case that maps names of merchants to their respective Merchant objects.
+ */
 public class MerchantMap {
     private final HashMap<String, Merchant> MERCHANT_MAP = new HashMap<>();
 
     /**
      * Constructs a MerchantMap object.
-     * @param merchantList a list of lists, each sublist contains the information for a single merchant
+     * @param merchantList a list of lists, each sublist contains the information for a single merchant represented as
+     *                     strings.
      */
     public MerchantMap(List<List<String>> merchantList){
         for (List<String> merchantInfo : merchantList) {
@@ -20,9 +24,9 @@ public class MerchantMap {
     }
 
     /**
-     * Get a Merchant object from the merchant map by indexing the merchant name.
-     * @param name a merchant name presented as a String
-     * @return a Merchant object
+     * Get a Merchant object from the merchant map by indexing the merchant's name.
+     * @param name a merchant name presented as a string.
+     * @return a Merchant object.
      */
     public Merchant getMerchant(String name){
         return MERCHANT_MAP.get(name);

24bd11c049f7655b1aba4cd6433de24d32a988d9
Jonah Mackey <jonahmackey699@gmail.com>
2021-12-04 16:16:44 -0500

    Simplified MerchantTest.

--- tcard/src/test/java/entities/MerchantTest.java
+++ tcard/src/test/java/entities/MerchantTest.java
@@ -6,64 +6,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MerchantTest extends TestCase {
-    String sampleDiscountInfo1;
-    String sampleDiscountInfo2;
     Discount sampleDiscount1;
     Discount sampleDiscount2;
     List<String> sampleMerchantInfo = new ArrayList<>();
-    String sampleMerchantName;
-    String sampleMerchantAddress;
-    String sampleMerchantHours;
-    String sampleMerchantDiscounts;
     Merchant sampleMerchant;
 
     public void setUp() throws Exception {
         super.setUp();
 
-        sampleDiscountInfo1 = "70:(textbooks/stationary/chips):(any).(MAT/PHY):(1/2).(APM/CS)";
-        sampleDiscountInfo2 = "20:(hats/sweaters):(any).(MAT/PHY):(1/2).(APM/CS)";
+        sampleDiscount1 = new Discount("70:(textbooks/stationary/chips):(any).(MAT/PHY):(1/2).(APM/CS)");
+        sampleDiscount2 = new Discount("20:(hats/sweaters):(any).(MAT/PHY):(1/2).(APM/CS)");
 
-        sampleDiscount1 = new Discount(sampleDiscountInfo1);
-        sampleDiscount2 = new Discount(sampleDiscountInfo2);
-
-        sampleMerchantName = "U of T Bookstore";
-        sampleMerchantAddress = "214 College Street";
-        sampleMerchantHours = "11am to 6pm";
-        sampleMerchantDiscounts = sampleDiscountInfo1 + "|" + sampleDiscountInfo2;
-
-        sampleMerchantInfo.add(sampleMerchantName);
-        sampleMerchantInfo.add(sampleMerchantAddress);
-        sampleMerchantInfo.add(sampleMerchantHours);
-        sampleMerchantInfo.add(sampleMerchantDiscounts);
+        sampleMerchantInfo.add("U of T Bookstore");
+        sampleMerchantInfo.add("214 College Street");
+        sampleMerchantInfo.add("11am to 6pm");
+        sampleMerchantInfo.add("70:(textbooks/stationary/chips):(any).(MAT/PHY):(1/2).(APM/CS)" + "|" + "20:(hats/sweaters):(any).(MAT/PHY):(1/2).(APM/CS)");
 
         sampleMerchant = new Merchant(sampleMerchantInfo);
     }
 
     public void testTestGetName() {
-        String expectedName = sampleMerchantName;
-
-        assertEquals(expectedName, sampleMerchant.getName());
+        assertEquals("U of T Bookstore", sampleMerchant.getName());
     }
 
     public void testGetAddress() {
-        String expectedAddress = sampleMerchantAddress;
-
-        assertEquals(expectedAddress, sampleMerchant.getAddress());
+        assertEquals("214 College Street", sampleMerchant.getAddress());
     }
 
     public void testGetHours() {
-        String expectedHours = sampleMerchantHours;
-
-        assertEquals(expectedHours, sampleMerchant.getHours());
+        assertEquals("11am to 6pm", sampleMerchant.getHours());
     }
 
     public void testGetDiscounts() {
         // To test that two discounts are equal, we check that their string representations and criteria are equal.
         List<String> expectedDiscounts = new ArrayList<>();
-        expectedDiscounts.add(sampleDiscount1.toString());
-        expectedDiscounts.add(sampleDiscount1.getCriteria());
-        expectedDiscounts.add(sampleDiscount2.toString());
-        expectedDiscounts.add(sampleDiscount2.getCriteria());
+        expectedDiscounts.add("-70% off on textbooks, stationary, and chips!");
+        expectedDiscounts.add("(any).(MAT/PHY):(1/2).(APM/CS)");
+        expectedDiscounts.add("-20% off on hats and sweaters!");
+        expectedDiscounts.add("(any).(MAT/PHY):(1/2).(APM/CS)");
 
         List<String> actualDiscounts = new ArrayList<>();
         for (Discount discount : sampleMerchant.getDiscounts()) {

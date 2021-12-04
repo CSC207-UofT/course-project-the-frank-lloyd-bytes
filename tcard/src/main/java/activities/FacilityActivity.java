6da5bd7d4828814b4d0f8245be190c85ec7bf362
ming0222 <mingqing.gao@mail.utoronto.ca>
2021-12-03 23:18:56 -0500

    small changes to facility

--- tcard/src/main/java/activities/FacilityActivity.java
+++ tcard/src/main/java/activities/FacilityActivity.java
@@ -23,7 +23,7 @@ public class FacilityActivity extends AppCompatActivity {
     FacilityManager facilityManager;
     UserManager userManager;
     FacilityAdapter adapter;
-    FileReader fileReader;
+    //FileReader fileReader;
 
     /**
      * starting the activity for the facility's page, programming the buttons on the screen as well as the adapter that
@@ -42,7 +42,7 @@ public class FacilityActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        fileReader = new FileReader();
+        //fileReader = new FileReader();
         facilitiesInfo = new ArrayList<>();
         try {
             setFacilityInfo();
@@ -70,11 +70,14 @@ public class FacilityActivity extends AppCompatActivity {
         facilityOne.add("St George");
         facilityOne.add("library");
         facilityOne.add("9 to 5");
+        facilityOne.add("program=(any),level=(undergrad):department=(any),position=(postdoc/professor)");
+
 
         facilityTwo.add("Robarts");
         facilityTwo.add("St George");
         facilityTwo.add("lab");
         facilityTwo.add("9 to 5");
+        facilityTwo.add("program=(CS),level=(undergrad):department=(CS),position=(postdoc/professor)");
         facilitiesInfo.add(facilityOne);
         facilitiesInfo.add(facilityTwo);
     }
